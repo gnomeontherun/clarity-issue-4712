9b3b4e3979ea3545cc7550993dd2b426555341ae
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-06-11 10:48:18 -0500

    add datagrid reproduction
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'issue4712';
+  data: any[] = [];
+  realData: any[] = [];
+  properties: any[] = [];
+
+  // Original ngOnInit
+  ngOnInit() {
+    this.properties = [
+      { name: "id", visible: true },
+      { name: "name", visible: true },
+      { name: "count", visible: true }
+    ];
+
+    interval(1000).subscribe(() => {
+      this.data = [];
+      this.data.push({
+        id: "" + Math.random(),
+        name: "Test" + 1,
+        count: "Count"
+      });
+
+      const count = 1 + Math.floor(Math.random() * Math.floor(5));
+      for (let i = 0; i < count; i++) {
+        this.data.push({
+          id: "" + i,
+          name: "Test" + 1,
+          count: "Count"
+        });
+      }
+    });
+  }
+
+  // Updated ngOnInit
+  // ngOnInit() {
+  //   this.properties = [
+  //     { name: "id", visible: true },
+  //     { name: "name", visible: true },
+  //     { name: "count", visible: true }
+  //   ];
+
+  //   setInterval(() => {
+  //     const data = [];
+      
+  //     data.push({
+  //       id: "" + Math.random(),
+  //       name: "Test" + 1,
+  //       count: "Count"
+  //     });
+      
+  //     const count = 1 + Math.floor(Math.random() * Math.floor(5));
+  //     for (let i = 0; i < count; i++) {
+  //       data.push({
+  //         id: "" + i,
+  //         name: "Test" + 1,
+  //         count: "Count"
+  //       });
+  //     }
+
+  //     this.data = data;
+  //   });
+  // }
+
+  trackById = (index, item) => item.id;
 }
